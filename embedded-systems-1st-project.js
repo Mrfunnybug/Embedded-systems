7ea0d2e88b972881119c56c1fd51115b22c1dd24
Omar V <omar.vazquezac@gmail.com>
2016-03-05 13:32:00 -0600

    added lm35 controller to fan

--- embedded-systems-1st-project.js
+++ embedded-systems-1st-project.js
@@ -1,9 +1,9 @@
 /*
 	Home automation with the following characteristics:
 		1.- It has 3 rooms, one living room, dining room, kitchen, bathroom, and a backyard
-		2.- The living room light must be activated both manually or automatic with the use of a photoresistor
-		3.- Every other rooms lights are controlled manually with a button
-		4.- It has a ventilation system using lm35 and a dc motor
+		->2.- The living room light must be activated both manually or automatic with the use of a photoresistor
+		->3.- Every other rooms lights are controlled manually with a button
+		->4.- It has a ventilation system using lm35 and a dc motor
 		5.- Backyard's light is controlled manually with a pushbutton and is an AC bulb
 		
 	This project can get improved but for time rank just the mentioned characteristics will be covered
@@ -12,13 +12,17 @@
 var five = require("johnny-five"),
 	arduino = five.Board();
 
-var living_room_light = false, other_rooms_light = false;
+var living_room_light = false, other_rooms_light = false, fan = false;
 
 var living_room_button, other_rooms_light_button;
 
 var living_room_light_pin, other_rooms_light_pin;
 
 var photoresistor_pin;
+
+var temperature;
+
+var fan_pin;
 
 arduino.on("ready", function() {
 	
@@ -51,10 +55,13 @@ arduino.on("ready", function() {
 		}
 	});
 
-
+	// All rooms excepting the living room are simultaneously light powered on manually	
 	other_rooms_light_button = five.Button(4);
 
+	// Light is powered via pin 12, LEDs connected in parallel
 	other_rooms_light_pin = new five.Pin(12);
+
+	// Change light state whenever 'other_lights_button' is pressed then released
 	other_rooms_light_button.on("release", function () {
 		other_rooms_light = !other_rooms_light;
 		if(other_rooms_light) {
@@ -67,5 +74,23 @@ arduino.on("ready", function() {
 		}
 	});
 
+	// Temperature will be measured with an LM35 sensor
+	temperature = new five.Thermometer({
+		controller: "LM35",
+		pin: "A1"
+	});
 
+	fan_pin = new five.Pin(5);
+
+	// Whenever temperature provided by LM35 sensor is greater than 22° C the fan input changes its value to 'high' and when temperature is less or equal to 22° C it goes 'low'
+	temperature.on("data", function () {
+		if(this.celsius > 22 && !fan) {
+			fan_pin.high();
+			fan = !fan;
+		}
+		else if(this.celsius <= 22 && fan) {
+			fan_pin.low();
+			fan = !fan;
+		}
+	})
 });
